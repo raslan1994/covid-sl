151f50c1364b69c9aa997de5ba84aa86ae812fda
raslan1994 <raslan1994@gmail.com>
2021-08-05 20:55:02 +0530

    base build

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import StatisticSummary from './StatisticSummary';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <StatisticSummary/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default function Header(props) {
+  return (
+    <div class="px-4 pt-5 my-5 text-center">
+    <h1 class="display-4 fw-bold">Covid Status</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+    </div>
+  </div>
+  );
+}
